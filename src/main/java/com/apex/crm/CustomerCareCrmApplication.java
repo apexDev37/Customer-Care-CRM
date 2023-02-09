b8c8f1f10033f9b148e2577d589ce11c68159a29
apexDev37 <mwangi.em37@gmail.com>
2023-02-09 09:42:47 +0300

    chore: Create project template
    Create spring boot 3 project template with a couple of required dependencies,
    viewable in the pom.xml file in the project root directory.

---
+++ src/main/java/com/apex/crm/CustomerCareCrmApplication.java
@@ -0,0 +1,13 @@
+package com.apex.crm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerCareCrmApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(CustomerCareCrmApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/apex/crm/CustomerCareCrmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apex.crm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerCareCrmApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
