c0a6a3ddda223aa54af12820a62d4900fc7f6694
apexDev37 <mwangi.em37@gmail.com>
2023-02-27 11:45:57 +0300

    feat: Make domain persistable
    Make the user domain persistable by adding JPA related annotations in order to persist the user domain using Spring Data JPA.

--- src/main/java/com/apex/crm/user/User.java
+++ src/main/java/com/apex/crm/user/User.java
@@ -1,13 +1,26 @@
 package com.apex.crm.user;
 
+import jakarta.persistence.*;
+import jakarta.annotation.Nonnull;
+
 import java.util.Set;
 import java.util.UUID;
 
+@Entity
+@Table(name = "crm_user")
 public class User {
+
+  @Id
   private UUID id;
   private String email;
   private String password;
+
+  @ElementCollection(fetch = FetchType.EAGER)
+  @Enumerated(EnumType.STRING)
+  @Nonnull
   private Set<UserRole> roles;
+
+  protected User() {}
 
   public User(UUID id,
               String email,
