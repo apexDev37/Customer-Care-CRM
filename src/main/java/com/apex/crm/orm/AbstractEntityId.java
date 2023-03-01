a62c6503fd666883fd0aa4a72ca7b7862ca5c168
apexDev37 <mwangi.em37@gmail.com>
2023-03-01 15:14:53 +0300

    feat: Add id/pk abstraction
    Add base abstract class: AbstractEntityId to provide an abstraction for all ID/primary key classes for all entities in the application.

---
+++ src/main/java/com/apex/crm/orm/AbstractEntityId.java
@@ -0,0 +1,49 @@
+package com.apex.crm.orm;
+
+import com.apex.orm.util.ArtifactForFramework;
+
+import jakarta.persistence.MappedSuperclass;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+import static com.google.common.base.MoreObjects.toStringHelper;
+
+@MappedSuperclass
+public abstract class AbstractEntityId<T extends Serializable>
+  implements Serializable {
+
+  private T id;
+
+  @ArtifactForFramework
+  protected AbstractEntityId() {}
+
+  protected AbstractEntityId(T id) {
+    this.id = Objects.requireNonNull(id);
+  }
+
+  public T getId() {
+    return id;
+  }
+  
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+
+    AbstractEntityId<?> that = (AbstractEntityId<?>) o;
+    return id.equals(that.id);
+  }
+
+  @Override
+  public int hashCode() {
+    return id.hashCode();
+  }
+
+  @Override
+  public String toString() {
+    return toStringHelper(this)
+            .add("id", id)
+            .toString();
+  }
+}
